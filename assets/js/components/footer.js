9955d175fd68b01f31176a9bf6ddeacad750bb1c
Memogatary <memogatary@gmail.com>
2025-09-23 23:43:36 -0600

    Update 250923

--- assets/js/components/footer.js
+++ assets/js/components/footer.js
@@ -113,7 +113,9 @@ class SiteFooterNewsletter extends HTMLElement {
             </div>
             <div>© ${year} Memogatary</div>
             <div>
-            <a href="/about/">About</a> · <a href="/contact/">Contact</a> · 
+            <a href="/about/">About</a> · <a href="/contact/">Contact</a>
+            </div>
+            <div>
             <a href="https://ko-fi.com/memogatary" target="_blank" rel="noopener" style="color:var(--accent); font-weight:600;">Buy me a coffee!</a>
             </div>
           </div>
